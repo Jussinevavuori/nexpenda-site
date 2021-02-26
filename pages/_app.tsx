7b5be418aafdff77cecf74b14a3b51f24cc7ca45
Jussi <jussi@nevavuori.fi>
2021-02-26 15:18:50 +0200

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,22 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from '@material-ui/core'
 import { theme } from '../styles/theme'
 import { Favicon } from '../components/Favicon/Favicon'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <ThemeProvider theme={theme}>
+		<Head>
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-SW3M4TBE5D" />
+			<script dangerouslySetInnerHTML={{
+				__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-SW3M4TBE5D');
+				`
+			}} />
+		</Head>
 		<Favicon />
 		<Component {...pageProps} />
 	</ThemeProvider>

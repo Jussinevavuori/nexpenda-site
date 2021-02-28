69d0e8bd067728bb911dbff54cf3cbdcc433a7f2
Jussi <jussi@nevavuori.fi>
2021-02-28 13:44:52 +0200

    New gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,17 +6,16 @@ export default class MyDocument extends Document {
 	render() {
 		return <Html>
 			<Head>
+
 				{/* Gtag */}
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-SW3M4TBE5D" />
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y74KDL8GJ9"></script>
 				<script dangerouslySetInnerHTML={{
 					__html: `
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-					gtag('config', 'G-SW3M4TBE5D', {
-						page_path: window.location.pathname,
-					});
-				`.replace(/\s+/g, " ").trim()
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-Y74KDL8GJ9');
+					`.replace(/\s+/g, " ").trim()
 				}} />
 
 			</Head>
